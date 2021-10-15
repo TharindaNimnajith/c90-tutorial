52e3768c05bd52157f0e51d85b9d6204539aee0b
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-16 00:15:31 +0530

    c90

---
+++ cmake-build-debug/CMakeFiles/c90_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c90_tutorial.dir/main.c.obj"
+  "c90_tutorial.exe"
+  "c90_tutorial.exe.manifest"
+  "c90_tutorial.lib"
+  "c90_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c90_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
